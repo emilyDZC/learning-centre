007e3604a2ca780354f99036bf676bc38576025f
Emily Chaplais <emilychaplais@hotmail.com>
2021-04-10 21:59:50 +0100

    Get database tests working

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,3 +1,19 @@
-module.exports.getSubjects = (req, res, next) => {
-  res.status(200).send({ subjects: {} });
+const Subject = require("../models/Subject");
+
+// @desc    Get all subjects
+// @route   GET /api/v1/subjects
+// @access  Public
+
+module.exports.getSubjects = async (req, res, next) => {
+  try {
+    const subjects = await Subject.find();
+
+    return res.status(200).json({
+      success: true,
+      count: subjects.length,
+      subjects: subjects,
+    });
+  } catch (error) {
+    return res.status(500).json({ success: false, error: error.message });
+  }
 };
--- spec/server.spec.js
+++ spec/server.spec.js
@@ -5,6 +5,7 @@ const { expect } = chai;
 const app = require("../server");
 const request = require("supertest");
 const connectDB = require("../config/db");
+const Subject = require("../models/Subject");
 
 connectDB();
 
@@ -13,15 +14,30 @@ describe("endpoints", () => {
   //   after(() => connection.destroy());
   describe("/api/v1/dashboard", () => {
     describe("GET", () => {
+      before((done) => {
+        Subject.deleteMany({}, done);
+        Subject.create({ name: "Psychology" });
+      });
+      after((done) => {
+        Subject.deleteMany({}, done);
+      });
       it("Status 200: should successfully connect to endpoint", () => {
         return request(app).get("/api/v1/dashboard").expect(200);
       });
-      it("Returns a list of subjects", () => {
+      it("Returns an array of subjects", () => {
         return request(app)
           .get("/api/v1/dashboard")
           .expect(200)
           .then(({ body }) => {
-            expect(body.subjects).to.be.an("object");
+            expect(body.subjects).to.be.an("array");
+          });
+      });
+      it("Returns the correct subjects", () => {
+        return request(app)
+          .get("/api/v1/dashboard")
+          .expect(200)
+          .then(({ body }) => {
+            expect(body.subjects[0].name).to.equal("Psychology");
           });
       });
     });
